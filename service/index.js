8d22d6302b27d20a7370b00c7d8dcd80862c6b9c
yosayon <yosa.yon@gmail.com>
2018-10-20 21:23:10 -0400

    added body-parser, added mongoose connection options into OPTS, created graphql schema, root, and Restaurant type, added pagination, text-search, filter options, sort option for root.searchText for graphql, marked graphiql as true for dev

--- service/index.js
+++ service/index.js
@@ -1,13 +1,74 @@
 require('dotenv').config()
-const mongoose = require('mongoose')
 const express = require('express')
-const winston = require('winston')
 const app = express()
+const mongoose = require('mongoose')
+const winston = require('winston')
+const helmet = require('helmet')
+const bodyParser = require('body-parser')
+const { Restaurant } = require('./restaurant')
+const OPTS = { family: 4, useNewUrlParser: true }
+const uri = `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@ds161614-a0.mlab.com:61614,ds161614-a1.mlab.com:61614/nyc-restaurants?replicaSet=rs-ds161614`
 
-require('./startup/logging')
-require('./startup/routes')(app)
-require('./startup/db')()
+const graphqlHTTP = require('express-graphql')
+const { graphql, buildSchema } = require('graphql')
 
+const schema = buildSchema(`
+  type Query {
+    allRestaurants: [Restaurant!]!
+    searchText(text: String!, filter: String, sort: String, pageSize: Int, pageNumber: Int) : [Restaurant!]!
+  }
+
+  type Restaurant {
+    name: String!
+    cuisine: String!
+    building: String!
+    street: String!
+    boro: String!
+    zipcode: String!
+    phone: String!
+    searchText: String!
+    imageUrl: String!
+    grade: String
+  }
+`)
+
+const escapeRegExp = s => s.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&")
+
+const root = {
+  allRestaurants: async function(){
+    const restaurants = await Restaurant.find({}).limit(10)
+    return restaurants
+  },
+  searchText: async function({ text, filter = "All", sort = "name", pageSize = 10, pageNumber = 2 }){
+    const filterGrade = {
+      A: { grade: "A" },
+      B: { grade: "B" },
+      C: { grade: "C" },
+      Z: { grade: "Z" },
+      other: { grade: {$in:["Not Yet Graded", null]}}
+    }
+    const string = new RegExp('\.\*'+text+'\.\*', 'i')
+    const restaurants = await Restaurant.find({ searchText: string})
+    .where(filterGrade[filter])
+    .skip((pageNumber - 1) * pageSize)
+    .limit(pageSize)
+    .sort(sort)
+    return restaurants
+  }
+}
+
+app.use(express.json())
+app.use(helmet())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}))
+
+mongoose.connect(uri, OPTS)
+  .then(() => winston.info('Connected to MongoDB...'))
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => winston.info(`Listening on port ${port}...`))
