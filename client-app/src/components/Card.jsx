c4204f4382879f8ebdbee6a9b338dc13e9538336
yosayon <yosa.yon@gmail.com>
2018-10-22 01:43:20 -0400

    created a card component for the restaurants page

--- client-app/src/components/Card.jsx
+++ client-app/src/components/Card.jsx
@@ -1,30 +1,38 @@
 import React from 'react'
 import sample from '../images/homepage.jpg'
+import Grade from './Grade'
+import './Card.css'
+
 const style = "height: 200px; width: 100%; display: block;"
+const width = "width: 18rem;"
+
 
 const Card = () => {
   return(
-    <div className="album py-5 bg-light">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-4">
-            <div className="card mb-4 box-shadow">
-              <img
-                className="card-img-top"
-                style={{style}}
-                src={sample}
-                data-holder-rendered="true"/>
-               <div className="card-body">
-                <p className="card-text">Los Tacos No. 1</p>
-                <p className="card-text">Mexican</p>
-                <p className="card-text">251 W 50th St, New York, NY 10019</p>
-                <p className="card-text">(212) 581-1818</p>
-               </div>
-              </div>
-            </div>
+    <div class="card-wrapper pb-5 px-3">
+      <div class="card" style={{width}}>
+        <img
+          className="card-img-top"
+          style={{style}}
+          src={sample}
+          data-holder-rendered="true"
+          alt="photo"
+        />
+        <div className="card-body">
+          <div className="card-title text-primary">
+            Los Tacos No. 1
           </div>
+          <p className="card-text">
+            Restaurant Name
+            <br />
+            Restaurant Address
+            <br />
+            Phone number
+          </p>
         </div>
       </div>
+    </div>
+
   )
 }
 
