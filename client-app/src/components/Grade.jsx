b9a132db1b2457f9e0b0a9fc790b95e0fa60a5e3
yosayon <yosa.yon@gmail.com>
2018-10-22 01:41:55 -0400

    added Card.css file, added a Grade component for the restaurants component

---
+++ client-app/src/components/Grade.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import seal from '../images/Nyc-seal-blue.png'
+import './ImageContainer.css'
+import './Grade.css'
+import a from '../images/a.png'
+import b from '../images/b.png'
+import c from '../images/c.png'
+import GP from '../images/GP.png'
+
+export default class Grade extends Component {
+  render(){
+    return(
+        <div className="grade-card">
+          <img
+            src={seal}
+            className="seal"
+          />
+          <img
+            src={a}
+            className="grade"
+          />
+        </div>
+    )
+  }
+}
